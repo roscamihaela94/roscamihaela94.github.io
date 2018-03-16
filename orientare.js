804e68f1e72a7bf1eac2184044c632c0509d2b18
roscamihaela94 <roscamihaela94@gmail.com>
2018-03-16 18:52:35 +0200

    Rotatie z

--- orientare.js
+++ orientare.js
@@ -3,4 +3,7 @@ window.addEvenimentListener("deviceorientation", on_divice_orientation)
 function on_divice_orientation(e)
 {
 	document.getElementById(id_orientation).innerHTML = e.alpha.toFixed(1)+ " " + e.beta.toFixed(1) + " " + e.gamma.toFixed(1);
+ 	var viewpoint = documen.getElementById("id_viewpoint");
+
+ 	viewpoint.setAttribute("orientation","0 0 1" + e.alpha * Math.PI / 180);
 }
