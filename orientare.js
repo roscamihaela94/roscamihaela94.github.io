3062170723a9433f1474615622914c90a6dc27e0
roscamihaela94 <roscamihaela94@gmail.com>
2018-03-16 18:36:25 +0200

    update

--- orientare.js
+++ orientare.js
@@ -2,5 +2,5 @@ window.addEvenimentListener("deviceorientation", on_divice_orientation)
 
 function on_divice_orientation(e)
 {
-	document.getElementById(id_orientation).innerHTML = e.alpha.toFixed(1)+ " " e.beta.toFixed(1) + " " + e.gamma.toFixed(1);
+	document.getElementById(id_orientation).innerHTML = e.alpha.toFixed(1)+ " " + e.beta.toFixed(1) + " " + e.gamma.toFixed(1);
 }
